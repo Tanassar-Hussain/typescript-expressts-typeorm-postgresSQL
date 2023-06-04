449b1cb88f5b4476e3ec1213082801269b0f88d1
tanassar-hussain <tanssarhussain@outlook.com>
2023-06-04 17:46:47 +0500

    one to one relation

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,51 @@
 import express from "express";
 import "reflect-metadata"
 import { DataSource } from "typeorm";
+import { User } from "./entites/User";
+import { Profile } from "./entites/Profile";
 
 
 const app = express();
 app.use(express.json());
 const port = 3000;
 
-app.get('/', (req, res)=>{
-    res.send('Hello From Express')
+app.get('/', async (req, res)=>{
+
+    const userRepo = AppDataSource.getRepository(User);
+    const profileRepo = AppDataSource.getRepository(Profile);
+    // Find All The Record
+    // const allRecord = await userRepo.find({relations: {profile: true}}); //lazy realtion 
+    // const allRecord = await userRepo.find(); //eager relation define in entity where relations are made
+    
+    // const userFound = await userRepo.findOne({where: {id: 3}})
+    //delete 
+    //await userRepo.delete(1) // pass ID
+
+    // let profile: Profile = new Profile();
+    // profile.gender = "Male";
+    // profile.photo = "This Is Photo";
+  
+    // //Insert user
+    // let user: User = new User();
+    // user.email = "Ali@gmail.com";
+    // user.firstName = "Ali";
+    // user.lastName = "Asad"
+    // user.profile = profile;
+
+    // const userInserted = await userRepo.save(user);
+
+    //update
+    // await userRepo.update(2, {firstName: "wwe" , lastName: "aew", email: "wwe@aew"});
+    
+    //filter Record
+    //const record = await userRepo.findOne({where:{firstName: "Tanassar"}})
+
+
+
+    //One to many - many to one
+
+
+    res.json('');
 });
 
 const AppDataSource = new DataSource({
--- src/entites/User.ts
+++ src/entites/User.ts
@@ -1,9 +1,10 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn} from "typeorm";
+import { Profile } from "./Profile";
 
 
 @Entity()
 export class User{
-    
+
     @PrimaryGeneratedColumn()
     id: number;
 
@@ -15,4 +16,8 @@ export class User{
 
     @Column()
     email: string;
+
+    @OneToOne(()=>Profile , {cascade : true, eager: true, onDelete: "CASCADE"}) //OnDelete Cascade means when user profile is deleted user will also be deleted...
+    @JoinColumn()
+    profile: Profile;
 }
